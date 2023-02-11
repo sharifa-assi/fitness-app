59e4980a2092dadd9ecd4be0b9a62672c7a31df7
sharifa-assi <sharifaalassi2001@gmail.com>
2023-02-11 18:09:31 +0200

    set up

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Routes,Route } from 'react-router-dom';
+import { Box } from '@mui/system';
+import { ExerciseDetail } from './pages/ExerciseDetail';
+import Home from './pages/Home';
+import { Footer } from './components/Footer';
+import { Navbar } from './components/Navbar';
+
+function App() {
+  return (
+ <Box width="400px" sx={{width:{xl:'1488px'}}}>
+<Navbar/>
+<Routes>
+  <Route path="/" element={<Home/>}/>
+  <Route path="/exercise/:id" element={<ExerciseDetail/>}/>
+</Routes>
+<Footer/>
+ </Box>
+  );
+}
+
+export default App;
